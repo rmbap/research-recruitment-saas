72bb5aefa295adcb31a9041afd3aad0b877bfde8
rmbap <152413413+rmbap@users.noreply.github.com>
2026-03-15 17:20:08 -0300

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,15 +10,20 @@ class DashboardController extends Controller
     public function __invoke(): View
     {
         $totalContacts = 0;
+        $validContacts = 0;
 
         if (DB::getSchemaBuilder()->hasTable('contacts')) {
             $totalContacts = DB::table('contacts')->count();
+
+            $validContacts = DB::table('contacts')
+                ->where('validation_status', 'valid')
+                ->count();
         }
 
         return view('dashboard', [
             'stats' => [
                 'total_contacts' => $totalContacts,
-                'valid_contacts' => 0,
+                'valid_contacts' => $validContacts,
                 'inconsistent_contacts' => 0,
                 'quality_rate' => 0,
             ],
